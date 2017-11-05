fe2b03bcbf480e91d169917cad7525f86bf0dd7d
Michele Dodič <michele.dodic@outlook.com>
2017-11-06 00:13:57 +0100

    HelloUser is added.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,14 @@
+import Java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.print("Please enter your name: ");
+		Scanner in = new Scanner(System.in);
+		String username = in.nextLine();	
+		HelloUser hu = new HelloUser(username);
+		hu.greetUser();
 	}
-
 }
